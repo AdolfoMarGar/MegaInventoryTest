7652d98247b4be3d7982942290349f726da41187
Adolfo Angel <adolfocod00@gmail.com>
2022-01-30 15:20:46 +0100

    Creating methods
    Methods to Post to the API

--- src/main/java/Services/ApiServices.java
+++ src/main/java/Services/ApiServices.java
@@ -1,4 +1,30 @@
 package Services;
 
+import javax.json.JsonObject;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class ApiServices {
+    String URL = "https://api.megaventory.com/v2017a";
+    Client client;
+    WebTarget target;
+    Invocation.Builder request;
+
+    public void startOperation(String endURL){
+        client = ClientBuilder.newClient();
+        target = client.target(URL+endURL);
+        request = target.request().accept(MediaType.APPLICATION_JSON);
+    }
+    
+    public void endOperation(){
+        client.close();
+    }
+
+    public Response requestPost(String jsonString,String endURL){
+        startOperation(endURL);
+        Response post = request.post(Entity.json(jsonString));
+
+        return post;
+    }
 }
