5cd11cfd6a71a22244e2058e2d5ef217f296bd2d
Adolfo Angel <adolfocod00@gmail.com>
2022-01-30 12:39:24 +0100

    Initial commit

---
+++ src/main/java/Interface/MainInterface.java
@@ -0,0 +1,16 @@
+package Interface;
+
+import javax.swing.*;
+
+public class MainInterface {
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+
+    public MainInterface() {
+
+    }
+
+}
---
+++ src/main/java/Modules/Apikey.java
@@ -0,0 +1,8 @@
+package Modules;
+
+public class Apikey {
+    String APIKEY = "53f477a210776068@m128069";
+
+    public Apikey() {
+    }
+}
